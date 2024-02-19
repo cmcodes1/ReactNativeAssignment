3cf214c4438a50ca96ef7d56ba47ccb58ef5fa8b
C M Pandey <chandramohan.pandey2015@gmail.com>
2024-02-19 16:48:19 +0530

    rendered static gifs

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  FlatList,
+  Image,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
+  View,
   useColorScheme,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {getTrendingGifs} from './src/api/getTrendingGifs';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -18,22 +20,59 @@ const App = () => {
     flex: 1,
   };
 
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await getTrendingGifs();
+    // console.log('response', response);
+    const gifs = response.data.map(item => {
+      return {
+        url: item.images.original.url.split('?')[0],
+        id: item.id,
+        title: item.title,
+      };
+    });
+    console.log('gifs', gifs);
+    setData(gifs);
+  };
+
+  const renderItem = ({item}) => (
+    <Image
+      style={styles.gif}
+      source={{
+        uri: item.url,
+      }}
+    />
+  );
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text style={styles.text}>Hello world!</Text>
-      </ScrollView>
+      <View style={styles.gifContainer}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 20,
+  gifContainer: {
+    flex: 1,
+  },
+  gif: {
+    height: 200,
+    width: 200,
   },
 });
 
---
+++ src/api/getTrendingGifs.js
@@ -0,0 +1,12 @@
+export const getTrendingGifs = async () => {
+  try {
+    const API_KEY = 'oPxq4IEL6uyO4WdgWkwnnpzzQOqQCOMd';
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`;
+    const response = await fetch(url);
+    const json = await response.json();
+    // console.log('json', json);
+    return json;
+  } catch (e) {
+    console.error('e', e);
+  }
+};
